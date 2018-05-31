27d2a9ed1e84be39c2b5fc2a4b75427dd4ee411e
Adila007 <future_adi1@mail.ru>
2018-05-31 17:17:49 -0500

    Aika

--- src/test/java/ticketmaster/step_definitions/Second.java
+++ src/test/java/ticketmaster/step_definitions/Second.java
@@ -3,6 +3,9 @@ package ticketmaster.step_definitions;
 public class Second {
 public static void main(String[] args) {
 	System.out.println("Hello");
+	System.out.println("Hello World");
+	System.out.println("Can I help you???");
+	
 
 }
 }
